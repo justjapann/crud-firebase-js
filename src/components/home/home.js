f797e5e08e096194e2ad86110f53ef1e2bbd36f6
japan <eduardobessadesouza@gmail.com>
2022-02-17 11:50:42 -0300

    final

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,21 +2,11 @@ import React, { useState, useEffect} from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import {auth} from '../../../config/firebaseconfig'
 import { db } from '../../../config/firebaseconfig';
-import {collection, query, orderBy, onSnapshot} from "firebase/firestore"
+import {collection, query, orderBy, onSnapshot, where, getDocs} from "firebase/firestore"
 
 export default function Home({navigation}){
 
     const user_id = auth.currentUser;
-
-   const [data, setData] = useState([{}]);
-
-    useEffect(
-        () =>
-          onSnapshot(collection(db, "data"), (snapshot) =>
-          setData(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
-          ),
-        []
-      );
 
     return(
         <View style={styles.container}>
@@ -25,21 +15,13 @@ export default function Home({navigation}){
                 <Text style={styles.text_perfil}>Perfil</Text>
             </View>
 
-            {data.map((datas) => (
-                <FlatList  key={datas.id}>
-                    <Text style={styles.data_text}>
-                Name: {datas.Name}
-                </Text>
-
-                <Text style={styles.data_text}>
-                Country: {datas.Country}
-                </Text>
-
+            <View style={styles.data}>
                 <Text style={styles.data_text}>
                 E-mail: {user_id.email}
                 </Text>
-          </FlatList>
-        ))}
+            </View>
+
+       
         </View>
     )}
 
